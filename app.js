24a29f5c385a940631e470bda57b9622b39522cf
Muccy98 <37148941+Muccy98@users.noreply.github.com>
2020-10-28 16:10:41 +0000

    Removed compression as it was breaking the app

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var compression = require('compression');
-var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" are of site
-
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
+
+app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
 var dev_db_url = "mongodb+srv://Jack:Fl0wba0t69%21@cluster0.qluf2.mongodb.net/local_library?retryWrites=true";
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+// var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var mongoDB = dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -27,12 +29,11 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(helmet());
+// app.use(compression);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(compression);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
